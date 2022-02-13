06ffab174048f2a6fdd9a684fb4f437300fcf44f
ogikun <g1621362@gmail.com>
2022-02-13 17:56:17 +0900

    added todoList View

--- lib/main.dart
+++ lib/main.dart
@@ -34,36 +34,42 @@ class TodoListPage extends StatefulWidget {
 }
 
 class _TodoListPageState extends State<TodoListPage> {
-  List<String> todoList = [];
-
+  List<DocumentSnapshot> documentList = [];
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('リスト一覧'),
+        actions: <Widget>[
+          ElevatedButton(
+            onPressed: () async {
+              final snapshot = await FirebaseFirestore.instance.collection('todoList').get();
+              setState(() {
+                documentList = snapshot.docs;
+              });
+            }, 
+            child: const Icon(Icons.refresh),
+          ),
+        ],
       ),
       body: ListView.builder(
-        itemCount: todoList.length,
-        itemBuilder: (context, index) {
+        itemCount: documentList.length,
+        itemBuilder: (BuildContext context, int index) {
           return Card(
             child: ListTile(
-              title: Text(todoList[index]),
+              title: Text(documentList[index]['text'].toString()),
             ),
           );
         },
       ),
       floatingActionButton: FloatingActionButton (
-        onPressed: () async {
-          final newListText = await Navigator.of(context).push(
+        onPressed: () {
+          Navigator.of(context).push(
             MaterialPageRoute(builder: (context) {
               return const TodoAddPage();
             }),
           );
-          if (newListText != null) { 
-            setState(() {
-              todoList.add(newListText);
-            });
-          }
         },
         child: const Icon(Icons.add),
       ),
@@ -106,7 +112,7 @@ class _TodoAddPageState extends State<TodoAddPage> {
               width: double.infinity,
               child:  ElevatedButton(
                 onPressed: () async {
-                  await FirebaseFirestore.instance.collection('todoList').add({'text': _text});
+                  await FirebaseFirestore.instance.collection('todoList').doc(Timestamp.now().millisecondsSinceEpoch.toString()).set({'text': _text});
                   Navigator.of(context).pop();
                 },
                 child: const Text('リスト追加', style: TextStyle(color: Colors.white)),
